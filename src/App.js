274f93b86f6704156f4489f99b560dc8426a1e52
Victor.K.Kosgei <37154833+victorkosgei254@users.noreply.github.com>
2019-06-17 14:40:11 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>KCL Landing Page</h2>
+    </div>
+  );
+}
+
+export default App;
